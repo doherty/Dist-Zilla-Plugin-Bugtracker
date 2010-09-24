48aa605928700e37f4b34ccb80030bfa3b48bb2b
Mike Doherty <doherty@cs.dal.ca>
2010-09-24 00:13:13 -0300

    Allow setting the tracker URL in dist.ini
    This retains the default behaviour of setting the rt.cpan.org URL
    and corresponding email address, but now allows setting a different
    URL and email address in dist.ini:
    
        [Bugtracker]
        web = http://github.com/me/My-Module/issues
    
    This resolves [rt.cpan.org #61602] Allow bug trackers other than RT

--- lib/Dist/Zilla/Plugin/Bugtracker.pm
+++ lib/Dist/Zilla/Plugin/Bugtracker.pm
@@ -8,21 +8,38 @@ package Dist::Zilla::Plugin::Bugtracker;
 use Moose;
 with 'Dist::Zilla::Role::MetaProvider';
 
+use MooseX::Types::URI qw[Uri];
+
+has web => (
+    is  => 'ro',
+    isa => Uri,
+    coerce => 1,
+    default => 'http://rt.cpan.org/Public/Dist/Display.html?Name=%s',
+);
+
+has mailto => (
+    is  => 'ro',
+    isa => 'Str',
+);
+
 sub metadata {
     my $self = shift;
 
-    my $web = sprintf('http://rt.cpan.org/Public/Dist/Display.html?Name=%s',
-      $self->zilla->name);
-    my $mailto = sprintf('bug-%s at rt.cpan.org', lc( $self->zilla->name ) );
-
-    return {
-        resources => {
-            bugtracker => {
-                web => $web,
-                mailto => $mailto,
-            }
+    my $web;
+    my $mailto;
+    my $ret;
+    if ($self->web) {
+        if ($self->web->as_string eq 'http://rt.cpan.org/Public/Dist/Display.html?Name=%s') {
+            $ret->{'resources'}->{'bugtracker'}->{'web'}    = sprintf($self->web->as_string, $self->zilla->name);
+            $ret->{'resources'}->{'bugtracker'}->{'mailto'} = sprintf('bug-%s at rt.cpan.org', lc($self->zilla->name));
         }
-    };
+        else {
+            $ret->{'resources'}->{'bugtracker'}->{'web'}    = $self->web->as_string;
+            $ret->{'resources'}->{'bugtracker'}->{'mailto'} = $self->mailto if $self->mailto;
+        }
+    }
+
+    return $ret;
 }
 __PACKAGE__->meta->make_immutable;
 no Moose;
